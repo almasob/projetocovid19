9952c1f8e289d08c279a1733fcf7489e1ee0623a
almasob <64939196+almasob@users.noreply.github.com>
2020-06-14 23:47:48 -0300

    Update scriptproject.js

--- scriptproject.js
+++ scriptproject.js
@@ -8,20 +8,20 @@ var formulario = document.getElementById("formularioSaude");
 
 formulario.addEventListener("submit", function(){
 	if(febre.checked && faltar.checked && dorpeito.checked && tosseseca.checked && dores.checked){
-	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
+	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!!\n Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
 	
 	else if(febre.checked && faltar.checked && dorpeito.checked && tosseseca.checked){
-	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
+	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
 	else if(febre.checked && faltar.checked && dorpeito.checked){
-	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
+	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
 	else if(febre.checked && faltar.checked && dorpeito.checked && dores.checked){
-	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
+	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
 	else if(febre.checked && faltar.checked && tosseseca.checked && dores.checked){
-	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
+	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
 	else if(febre.checked && dorpeito.checked && tosseseca.checked && dores.checked){
-	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
+	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
 	else if(faltar.checked && dorpeito.checked && tosseseca.checked && dores.checked){
-	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
+	alert("ATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!\nATENÇÂO!!! Vá ao médico imediatamente, você pode estar INFECTADO!")}
 	else if(febre.checked || faltar.checked || dorpeito.checked || tosseseca.checked || dores.checked){
 	alert("Você tem sintomas leves... Mantenha-se distante das pessoas por pelo menos 15 dias... Se persistir, procure um MÉDICO!!\nVocê tem sintomas leves... Mantenha-se distante das pessoas por pelo menos 15 dias... Se persistir, procure um MÉDICO!!\nVocê tem sintomas leves... Mantenha-se distante das pessoas por pelo menos 15 dias... Se persistir, procure um MÉDICO!!")}
 	else(alert("Muito bom!!!!\nVocê está livre de sintomas... Continue se previnindo, em breve estaremos todos livres!!"));
